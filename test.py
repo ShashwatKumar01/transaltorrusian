a1a10d5ab3c29ded9af5244046a2244ba217cf11
Shashwat  Kumar Das <121427522+ShashwatKumar01@users.noreply.github.com>
2024-09-03 14:38:51 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,31 @@
+
 import asyncio
-from pyrogram import Client, filters
-from googletrans import Translator
+
+from pyrogram import Client, filters, enums
+from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
 import logging
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import errors
+from quart import Quart
+from googletrans import Translator
+
 
 api_id = '26566076'
 api_hash = '40ce27837b95819c42cac67b46a2dc2b'
-bot_token = '7132391493:AAEYNvmCqcjtKy-3CwenJBTiIysXp0El0jo'
+bot_token='7132391493:AAEYNvmCqcjtKy-3CwenJBTiIysXp0El0jo'
 
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+
+bot = Quart(__name__)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 translator = Translator()
+
+@bot.route('/')
+async def hello():
+    return 'Hello, world!'
+
 
 @app.on_message(filters.command("start") & filters.private)
 async def start(client, message):
@@ -60,5 +74,22 @@ async def translate_media_message(client, message):
     else:
         logger.warning(f"Unsupported media type: {message.media}")
 
+
+@bot.before_serving
+async def before_serving():
+    await app.start()
+
+
+@bot.after_serving
+async def after_serving():
+    await app.stop()
+
+
+# if __name__ == '__main__':
+
+# bot.run(port=8000)
 if __name__ == '__main__':
-    app.run()
+    loop = asyncio.get_event_loop()
+    loop.create_task(bot.run_task(host='0.0.0.0', port=8080))
+    loop.run_forever()
+
